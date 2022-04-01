8d08f16235e55b2c0d211688bac51561225d4a6f
GodOfChess <CreativeOrange1@yandex.ru>
2022-04-01 10:27:13 +0500

    Добавлена мини игра
    Добавлена мини игра "Камень-ножницы-бумага"

---
+++ GodOfChessSite/Helpers/GameHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GodOfChessSite.Helpers
+{
+    public class GameHandler
+    {
+        public RSPOptions Choose { get; set; }
+        public RSPOptions WinCondition { get; set; }
+        public RSPOptions LoseCondition { get; set; }
+        public string Image { get; set; }
+
+        public GameState GameResult(GameHandler opponent)
+        {
+            if (Choose == opponent.Choose)
+                return GameState.Draw;
+            else if (LoseCondition == opponent.Choose)
+                return GameState.Victory;
+            else return GameState.Lose;
+        }
+    }
+}
---
+++ GodOfChessSite/Helpers/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GodOfChessSite.Helpers
+{
+    public enum GameState
+    {
+        Victory,
+        Draw,
+        Lose
+    }
+}
